5ff9d7bfd5b11d63d7de788e21fdb94aa7bc05fc
Osvaldo Vasquez <ot.vasquez99@gmail.com>
2022-01-19 10:37:21 -0800

    null

--- js/data-processing.js
+++ js/data-processing.js
@@ -6,3 +6,13 @@ console.log(queryString);
 
 //separate querystring parameters
 const urlParams = new URLSearchParams(queryString);
+
+let myData = '';
+urlParams.forEach(function(value, key) {
+    key = key.split("_")
+    myData += `<p>${key}: ${value}</p>`;
+});
+
+myData += `<p><a href="index.html` > CLEAR < /a></p > ;
+
+document.getElementById("output").innerHTML = myData;
