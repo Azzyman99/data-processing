1f50e33496f2e0242b4996d140f333398c6ed2fd
Osvaldo Vasquez <ot.vasquez99@gmail.com>
2022-01-19 10:41:20 -0800

    commit all

--- js/data-processing.js
+++ js/data-processing.js
@@ -2,17 +2,20 @@
 const queryString = window.location.search;
 
 //output to console    
-console.log(queryString);
+//console.log(queryString);
 
 //separate querystring parameters
 const urlParams = new URLSearchParams(queryString);
 
 let myData = '';
 urlParams.forEach(function(value, key) {
-    key = key.split("_")
+
+    //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
+    // will replace underscore with spaces
+    key = key.split("_").join(" ");
+
     myData += `<p>${key}: ${value}</p>`;
+    //console.log(value, key);
 });
-
-myData += `<p><a href="index.html` > CLEAR < /a></p > ;
-
+myData += `<p><a href="index.html">CLEAR</a></p>`;
 document.getElementById("output").innerHTML = myData;
